253f84f5817f3812a499a642b7ab52e3a21e9994
cxobaidullah <obaidullah70783@gmail.com>
2023-02-26 17:34:17 +0500

    addd db connection

--- connection.inc.php
+++ connection.inc.php
@@ -2,5 +2,5 @@
 if(!isset($_SESSION)){
     session_start();
 }
-$con=mysqli_connect("localhost","root","","realestate");
+$con=mysqli_connect("localhost","u345162108_realstate","8~P2RDSp","u345162108_realstate");
 ?>
