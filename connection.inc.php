5bf41ad5f9a38590bbebb106f4e701125520f682
cxobaidullah <obaidullah70783@gmail.com>
2023-03-14 23:22:55 +0500

    header and connection file changes again

--- connection.inc.php
+++ connection.inc.php
@@ -2,6 +2,6 @@
 if(!isset($_SESSION)){
     session_start();
 }
-$con=mysqli_connect("localhost","root","","realestate");
-// $con=mysqli_connect("localhost","u345162108_realstate","Fz0T3xgqm$","u345162108_realstate");
+// $con=mysqli_connect("localhost","root","","realestate");
+$con=mysqli_connect("localhost","u345162108_realstate","Fz0T3xgqm$","u345162108_realstate");
 ?>
